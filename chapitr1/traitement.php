526e37d7add019f2bf40b86bf9825bb007ec9327
yousraS12 <boujeab2020@gmail.com>
2026-02-18 09:13:07 +0000

    creat fiels

---
+++ chapitr1/traitement.php
@@ -0,0 +1,23 @@
+<form action="traitement.php" method="POST">
+    <label>Nom :</label>
+    <input type="text" name="nom"><br>
+
+    <label>Email :</label>
+    <input type="email" name="email"><br>
+
+    <button type="submit">Envoyer</button>
+</form>
+<?php
+// fichier : traitment.php
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $nom = trim($_POST['nom']);
+    $email = trim($_POST['email']);
+
+    if (empty($nom) || empty($email)) {
+        echo "Tous les champs sont obligatoires.";
+    } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "Email invalide.";
+    } else {
+        echo "Bonjour $nom, votre email est $email.";
+    }
+}
---
+++ chapitr5/tst.php
@@ -0,0 +1,8 @@
+<?php
+$premier_livre = 20 ; 
+$deuxiéme_livre = 40 ; 
+$total = premier_livre + deuxiéme_livre ; 
+ 
+
+
+?>
